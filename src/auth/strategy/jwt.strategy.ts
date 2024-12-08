f0b41fda6e0756fbd6a968637219c5683408319f
Foster Z <fosterunlimited0123@gmail.com>
2024-12-08 18:27:31 +0530

    feat: add jwt strategy to auth component

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,10 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from 'passport-jwt'
+export class JwtStrategy extends PassportStrategy(Strategy){
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretDrKey: process.env.JWT_SECRET
+        })
+    }
+}

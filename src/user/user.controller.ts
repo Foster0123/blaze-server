1b8d00cc76dc1a1f961ac5f6d5022bed720ab51d
Foster Z <72973015+Foster0123@users.noreply.github.com>
2025-06-23 18:48:31 +0530

    feat(user authentication): add user authentication with jwt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor (private userService : UserService) {}
+    @UseGuards(JwtGuard)
+    @Get('account')
+        redirectToAccount (@Req() req : Request) {
+            return req.user
+        }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Req } from '@nestjs/common';
+import { Request } from 'express';
 @Injectable()
-export class UserService {}
+export class UserService {
+    
+}
